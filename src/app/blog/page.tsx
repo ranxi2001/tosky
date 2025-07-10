4fbfd07d4bcc5ff0e9ed274cdcaa13cf2da608cc
ranxi2001 <ranxi169@163.com>
2025-07-10 21:49:51 +0800

    add：新增博客获取检查

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -12,6 +12,9 @@ const Page = async (
     const searchParams = await props.searchParams;
     const page = searchParams.page ? parseInt(searchParams.page as string) : 1;
     const result = await wisp.getPosts({ limit: 6, page });
+
+    console.log("getPosts result:", result); // 检查是否获取到博客
+
     return (
         <div className="container mx-auto px-5 mb-10">
             <Header />
